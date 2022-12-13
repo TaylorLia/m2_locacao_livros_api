0a736d3a389111cf3b6024c7a7f5f845158061cb
André lucas farias <88065639+TaylorLia@users.noreply.github.com>
2022-12-13 07:32:52 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Atividade M2 - API
+## SISTEMA DE APOIO A DECISÃO + TÓPICOS EM DESENVOLVIMENTO DE SISTEMAS DE INFORMAÇÃO
+
+### Aluno:
+#### André Lucas Farias
+
+Comandos para executar o projeto:
+
+* npm install
+* npm start
+
+#### Repositório Front-end: https://github.com/TaylorLia/m2_locacao_livros_front
+#### Repositório ChatBot: https://github.com/TaylorLia/m2_chatbot_loca-o_livros
---
+++ index.js
@@ -0,0 +1,31 @@
+// definindo o strict mode
+'use strict'
+
+// Definindo imports
+const express = require('express')
+const cors = require('cors')
+const config = require('./config')
+const usersRoutes = require('./routes/users-routes')
+const booksRoutes = require('./routes/books-routes')
+const hiresRoutes = require('./routes/hires-routes')
+
+// Inicializando o Express
+const app = express()
+
+// Definindo a utilização de JSON no corpo da requisição
+// (Body Parser)
+app.use(express.json())
+
+// Definindo a utilização do CORS
+// (frontend)
+app.use(cors())
+
+// utilizando as rotas
+app.use('/api/users', usersRoutes.routes)
+app.use('/api/hires', hiresRoutes.routes)
+app.use('/api/books', booksRoutes.routes)
+
+// Definindo a porta onde o servidor estará ouvindo
+app.listen(config.port, () =>
+  console.log('API está rodando em http://localhost:' + config.port)
+)
